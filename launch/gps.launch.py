4c961f536c389ec77ba1179e47a49988cc0d5d97
autumnpeterson24 <petersonautumn24@gmail.com>
2026-02-15 14:47:38 -0700

    gps waypoint follower updates

--- launch/gps.launch.py
+++ launch/gps.launch.py
@@ -11,6 +11,21 @@ Check your nodes are working and publishing:
     ros2 topic echo safety/estop
 
     *should see a bunch of data:true*
+
+
+    ROS commands to run: 
+    ros2 topic echo /gps/fix
+    ros2 topic echo /gps/cog_deg
+        * Direction PAVBot is moving across the earth (0 deg = North, 90 deg = East, 180 deg = South, 270 deg = West)
+        * only computed when moving so not good for stationary will change when have IMU
+        * Use as heading for testing
+
+    ros2 topic echo /gps/sog_mps
+        * SOG: speed over ground (mps)
+        * 1 knot = 0.514444 m/s
+        * 
+
+
 """
 
 from launch import LaunchDescription
@@ -25,7 +40,7 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'port': '/dev/ttyUSB0',
-                'baud': 115200,
+                # 'baud': 115200,
                 'timeout_ms': 500
             }]
         )
